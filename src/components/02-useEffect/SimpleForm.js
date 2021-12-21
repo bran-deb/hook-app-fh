f0a2d670548d0d8c2e0b631a8dfa626e2184bee1
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-21 00:37:41 -0400

    Added useEffect SimpleForm
    - added useeffect in all inputs
    - added simple css

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './effects.css'
 
 export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+    })
+
+    const { name, email } = formState
+
+    useEffect(() => {
+        console.log('hey')
+    }, [])
+
+    useEffect(() => {
+        console.log('cambio formulario')
+    }, [formState])
+
+    useEffect(() => {
+        console.log('cambio name')
+    }, [name])
+
+    useEffect(() => {
+        console.log('cambio email')
+    }, [email])
+
+
+    const handleInputChange = ({ target }) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    }
+
+
     return (
         <>
             <h1>useEffect</h1>
             <hr />
+            <div className='form-group'>
+                <input
+                    type='text'
+                    name='name'//elemento del objeto para el match
+                    className='form-control'
+                    placeholder='Tu nombre'
+                    autoComplete='off'
+                    value={name}
+                    onChange={handleInputChange}
+                />
+            </div>
 
+            <div className='form-group'>
+                <input
+                    type='text'
+                    name='email'//elemento del objeto para el match
+                    className='form-control'
+                    placeholder='email@gmail.com'
+                    autoComplete='off'
+                    value={email}
+                    onChange={handleInputChange}
+                />
+            </div>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <CounterWithCustomHook />,
+  <SimpleForm />,
   document.getElementById('root')
 );
