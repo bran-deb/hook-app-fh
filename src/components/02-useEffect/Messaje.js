1685741512f7eb42cd4060052d7cf5a783cb8eef
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-21 20:45:42 -0400

    Added unmount to useEffect
    - se añadio el unmount para no sobrecargar los effects

--- src/components/02-useEffect/Messaje.js
+++ src/components/02-useEffect/Messaje.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import { useEffect } from 'react/cjs/react.development'
+import React, { useState, useEffect } from 'react'
 
 export const Messaje = () => {
 
+    const [coords, setCoords] = useState({ x: 0, y: 0 })
+    const { x, y } = coords
+
     useEffect(() => {
-        console.log('componente montado')
+
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y }
+            setCoords(coords)
+        }
+
+        //para no mandar el callback mandamos la funcion mouseMove
+        window.addEventListener('mousemove', mouseMove)
+
         return () => {
-            console.log('componente desmontado')
+            //desmontamos para que no se agrupen los effects
+            window.removeEventListener('mousemove', mouseMove)
         }
     }, [])
 
     return (
         <div>
             <h3>Eres genial</h3>
+            <p>
+                x:{x}y:{y}
+            </p>
         </div>
     )
 }
