7c557131a79f56d33c1ffd55e2e62d2aaa0824c1
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-23 23:56:58 -0400

    Real example Ref

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
@@ -9,7 +10,7 @@ import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <FocusScreen />,
+  <RealExampleRef />,
   document.getElementById('root')
 
 );
