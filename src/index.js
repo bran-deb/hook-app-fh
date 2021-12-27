63b34ad7eb777165c0e5fe6c5de5d23386401157
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-27 17:56:45 -0400

    added Method React memo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Memorize } from './components/06-memos/Memorize';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
@@ -11,7 +12,7 @@ import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <Layout />,
+  <Memorize />,
   document.getElementById('root')
 
 );
