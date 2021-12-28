624bc05097e3171fc26fe1c7c93a52d547531304
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-28 01:17:06 -0400

    added useMemo in function

---
+++ src/helpers/procesopesado.js
@@ -0,0 +1,6 @@
+export const procesoPesado = (iteraciones) => {
+    for (let i = 0; i < iteraciones; i++) {
+        console.log('ahi vamos...')
+    }
+    return `${iteraciones} iteraciones validadas.`
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Memorize } from './components/06-memos/Memorize';
+import { MemoHook } from './components/06-memos/MemoHook';
+// import { Memorize } from './components/06-memos/Memorize';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
@@ -12,7 +13,7 @@ import { Memorize } from './components/06-memos/Memorize';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <Memorize />,
+  <MemoHook />,
   document.getElementById('root')
 
 );
