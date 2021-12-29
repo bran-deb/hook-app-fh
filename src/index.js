d9db7488bbe626456c13a0321718dc17dd0ab6f0
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-28 22:28:54 -0400

    added tarea memorize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memorize/Padre';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { Memorize } from './components/06-memos/Memorize';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
@@ -14,7 +15,7 @@ import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <CallbackHook />,
+  <Padre />,
   document.getElementById('root')
 
 );
