aca01be4448a48f474b0c53786268e8c0a3b719f
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-01-02 23:31:27 -0400

    Added useReducer to control state
    - agregado de styles basicos
    - agregado de todoApp
    - agregado de el todoReducer para el state de todoApp

---
+++ src/components/08-useReducer/todoReducer.js
@@ -0,0 +1,11 @@
+export const todoReducer = (state = [], action) => {
+    switch (action.type) {
+        // case 'add':
+
+        //     break;
+
+        //retorna el valor inicial
+        default:
+            return state
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { TodoApp } from './components/08-useReducer/TodoApp';
 // import { Padre } from './components/07-tarea-memorize/Padre';
-// // import { CallbackHook } from './components/06-memos/CallbackHook';
-// // import { MemoHook } from './components/06-memos/MemoHook';
-// // import { Memorize } from './components/06-memos/Memorize';
-// // import { Layout } from './components/05-useLayoutEffect/Layout';
-// // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-// // import { FocusScreen } from './components/04-useRef/FocusScreen';
-// // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-// // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-// // import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// // import { CounterApp } from './components/01-useState/CounterApp';
-// // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-// // import { HookApp } from './HookApp';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { MemoHook } from './components/06-memos/MemoHook';
+// import { Memorize } from './components/06-memos/Memorize';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { HookApp } from './HookApp';
+// import './components/08-useReducer/intro-reducer'
 
-// ReactDOM.render(
-//   <Padre />,
-//   document.getElementById('root')
+ReactDOM.render(
+    <TodoApp />,
+    document.getElementById('root')
 
-// );
-
-import './components/08-useReducer/intro-reducer'
+);
