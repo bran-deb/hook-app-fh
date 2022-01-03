11633acb3ee511ce3f06d705389de7f051ece231
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-01-03 12:20:48 -0400

    added form

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -17,14 +17,48 @@ export const TodoApp = () => {
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length})</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className='row'>
+                <div className='col-7'>
+                    <ul className='list-group list-group-flush'>
+
+                        {
+                            todos.map((todo, i) => (
+                                <li
+                                    key={todo.id}
+                                    className='list-group-item'
+                                >
+                                    <p className='text-center'>{i + 1}. {todo.desc}</p>
+                                    <button className='btn btn-danger'>
+                                        borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+
+                    </ul>
+                </div>
+
+                <div className='col-5'>
+                    <h4>agregar TODO</h4>
+                    <hr />
+
+                    <form>
+                        <input
+                            type='text'
+                            name='description'
+                            className='form-control'
+                            placeholder='Aprender ...'
+                            autoComplete='off'
+                        />
+                        <button className='btn btn-outline-primary mt-1 btn-block'>
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
