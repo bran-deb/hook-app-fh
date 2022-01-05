668ab84ed81abfa1903d4445dee1d5e986b9c7a6
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-01-04 20:30:20 -0400

    added navbar

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -2,8 +2,11 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Routes,
-    Route
+    Route,
+    Navigate,
+
 } from "react-router-dom";
+import { NavBar } from './NavBar';
 import { HomePage } from './HomePage';
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
@@ -12,10 +15,12 @@ import { LoginScreen } from './LoginScreen';
 export const AppRouter = () => {
     return (
         <Router>
+            <NavBar />
             <Routes>
                 <Route exact path='/' element={<HomePage />} />
                 <Route exact path='/about' element={<AboutScreen />} />
                 <Route exact path='/login' element={<LoginScreen />} />
+                <Route path='/' element={<Navigate replace to='/' />} />
             </Routes>
         </Router>
     )
---
+++ src/components/09-useContext/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark" >
+            <div className="container-fluid" >
+                <Link className="navbar-brand" to='/' > UseContext</Link >
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup" >
+                    <div className="navbar-nav" >
+                        <NavLink className="nav-link" to='/' > Home</NavLink >
+                        <NavLink className="nav-link" to='/about' > About</NavLink >
+                        <NavLink className="nav-link" to='/login' > Login</NavLink >
+                    </div >
+                </div >
+            </div >
+        </nav >
+    )
+}
