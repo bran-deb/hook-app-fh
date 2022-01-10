d10234aba745add53b1215e767b7429c330dfddc
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-01-10 00:55:09 -0400

    implemented useCounter mock

--- src/tests/components/03-examples/MultipleCustomHooks.test.js
+++ src/tests/components/03-examples/MultipleCustomHooks.test.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme'
 import { MultipleCustomHooks } from "../../../components/03-examples/MultipleCustomHooks"
 import { useFetch } from '../../../hooks/useFetch';
-//simulamos el usefetch
+import { useCounter } from '../../../hooks/useCounter';
+
+//simulamos el usefetch y useCounter
 jest.mock('../../../hooks/useFetch')
+jest.mock('../../../hooks/useCounter')
 
 
 describe('Pruebas en multipleCustomHooks', () => {
+
+    beforeEach(() => {
+        useCounter.mockReturnValue({
+            counter: 10,
+            increment: () => { }
+        });
+    })
 
     test('debe de mostrarse correctamente', () => {
         //el usefetch regresa el estado inicial o por defecto
@@ -32,9 +42,9 @@ describe('Pruebas en multipleCustomHooks', () => {
 
         const wrapper = shallow(<MultipleCustomHooks />)
         //buscamos un elemento por su clase en el wrapper [alert]
-        expect(wrapper.find('.alert').exists()).toBe(false)
-        expect(wrapper.find('.mb-4').text().trim()).toBe('Hola Mundo')
-        expect(wrapper.find('footer').text().trim()).toBe('jairo')//footer es una etiqueta
+        expect(wrapper.find('.alert').exists()).toBe(false)             //no existe loading en la clase alert
+        expect(wrapper.find('.mb-4').text().trim()).toBe('Hola Mundo')  //la clase mb-4 contiene el quote
+        expect(wrapper.find('footer').text().trim()).toBe('jairo')      //footer es una etiqueta y contiene author
         console.log(wrapper.html())
     })
 
